faceea4e9da499c1add7a87002c6b1cc735f8845
Heezoni <68049856+Heezoni@users.noreply.github.com>
2021-12-17 20:25:18 +0900

    Add files via upload

---
+++ teaching.js
@@ -0,0 +1,68 @@
+// $(function () {
+//     $('.item1').hover(function () {
+//         $('.item > .num').text("01");
+//         $('.item > title').text(UX 리서치);
+//         $('.item > p').text(내용을 입력하세요.);
+//     });
+
+//     $('.item2').hover(function () {
+//         $('.item > .num').text("02");
+//         $('.item > title').text(UX 리서치);
+//         $('.item > p').text(내용을 입력하세요.);
+//     });
+
+//     $('.item3').hover(function () {
+//         $('.item > .num').text("3");
+//         $('.item > title').text(UX 리서치);
+//         $('.item > p').text(내용을 입력하세요.);
+//     });
+// });
+
+$('.item1').hover(function () {
+    $('.item > .num').text("01");
+    $('.item > .title').text("UX 리서치");
+    $('.item > .title').css("font-weight", "500");
+    $('.item > p').text("by UX 컨설턴트 전민수");
+});
+
+$('.item2').hover(function () {
+    $('.item > .num').text("02");
+    $('.item > .title').text("UX 트렌드");
+    $('.item > .title').css("font-weight", "500");
+    $('.item > p').text("by UX 컨설턴트 전민수");
+});
+
+$('.item3').hover(function () {
+    $('.item > .num').text("03");
+    $('.item > .title').text("UX 자료실");
+    $('.item > .title').css("font-weight", "500");
+    $('.item > p').text("by UX 컨설턴트 전민수");
+});
+
+$('.item4').hover(function () {
+    $('.item > .num').text("04");
+    $('.item > .title').text("디자인 영감얻기");
+    $('.item > .title').css("font-weight", "500");
+    $('.item > p').text("by 비범한츈");
+});
+
+$('.item5').hover(function () {
+    $('.item > .num').text("05");
+    $('.item > .title').text("디자인 뉴스레터");
+    $('.item > .title').css("font-weight", "500");
+    $('.item > p').text("by 김강령");
+});
+
+$('.item6').hover(function () {
+    $('.item > .num').text("06");
+    $('.item > .title').text("커머스 디자인");
+    $('.item > .title').css("font-weight", "500");
+    $('.item > p').text("by 이선주");
+});
+
+$('.item7').hover(function () {
+    $('.item > .num').text("07");
+    $('.item > .title').text("월간 디자인");
+    $('.item > .title').css("font-weight", "500");
+    $('.item > p').text("by 월간디자인");
+});
